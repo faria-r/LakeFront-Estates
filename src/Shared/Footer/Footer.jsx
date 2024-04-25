44362dcc0b4b42a28dfedf5f1e5c659c66433002
faria-r <fariacse11@gmail.com>
2024-04-25 11:15:38 +0600

    footer first section developed

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,37 +1,63 @@
 import React from "react";
+import { FaMapLocationDot } from "react-icons/fa6";
+import {
+  MdHomeWork,
+  MdOutlineMailOutline,
+  MdRealEstateAgent,
+} from "react-icons/md";
+import { PiPhoneDisconnectBold } from "react-icons/pi";
 
 const Footer = () => {
   return (
     <div className="w-[90vw] mx-auto mt-8">
       <h2 className="text-3xl my-6">LakeFront Real Estates</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-3 w-[75vw] p-6">
-        <div >
-          <h2 className="text-xl">Lake Oconee Real Estate Expert</h2>
-        </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 w-[80vw] p-6 border items-start  font-sans">
         <div>
-          <div className="lg:flex justify-around items-center">
-            <p>icon</p>
+          <h2 className="text-xl lg:mt-6">Lake Oconee Real Estate Expert</h2>
+        </div>
+        <div className="flex-col justify-center font-bold">
+          <div className="lg:flex justify-around items-center my-6">
+            <p className="text-3xl">
+              <MdOutlineMailOutline />
+            </p>
             <p>
               EMAIL <br />
-             FARIARAHMAN622@GMAIL.COM
+              FARIARAHMAN622@GMAIL.COM
             </p>
           </div>
-          <div className="lg:flex justify-around items-center">
-            <p>icon</p>
+          <div className="lg:flex justify-around items-center my-6">
+            <p className="text-3xl">
+              <FaMapLocationDot />
+            </p>
             <p>
               ADDRESS <br />
-              1011 PARK LANE GREENSBORO, GA 30642
+              FARIARAHMAN622@GMAIL.COM
             </p>
           </div>
-          <p>QUICK LINKS</p>
+          <p className="lg:ml-[25%]">QUICK LINKS</p>
         </div>
-        <div className="lg:flex justify-around items-center">
-            <p>icon</p>
-            <p>
-              PHONE NUMBER <br />
-              1011 PARK LANE GREENSBORO, GA 30642
-            </p>
+        <div className="lg:ml-[10%] lg:flex justify-around items-center my-6 font-bold">
+          <p className="text-3xl -mr-16">
+            <PiPhoneDisconnectBold />
+          </p>
+          <p>
+            PHONE - NUMBER <br />
+            +88 01970-477973
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 p-4 w-[90vw] border">
+        <div className="text-2xl">Lakefront Estates</div>
+        <div className="lg:-ml-[25%]">
+          <h2 className="my-4">
+            All information deemed reliable but not guaranteed and should be
+            independently reviewed and verified.
+          </h2>
+          <div className="flex justify-start text-4xl gap-4">
+            <MdRealEstateAgent />
+            <MdHomeWork />
           </div>
+        </div>
       </div>
     </div>
   );
