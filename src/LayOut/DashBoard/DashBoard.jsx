e42e27d147450ac30f497b08e72e7cd0822169a0
faria-r <fariacse11@gmail.com>
2024-04-30 18:12:48 +0600

    Admin dashboard layout designed

--- src/LayOut/DashBoard/DashBoard.jsx
+++ src/LayOut/DashBoard/DashBoard.jsx
@@ -1,36 +1,61 @@
-import React from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
-import Navber from '../../Shared/Navber/Navber';
-import { FaHome } from 'react-icons/fa';
-import { AiFillHome } from 'react-icons/ai';
+import React from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import Navber from "../../Shared/Navber/Navber";
+import { FaHome } from "react-icons/fa";
+import { AiFillHome, AiTwotoneSchedule } from "react-icons/ai";
+import { FaHouse } from "react-icons/fa6";
+import { ImUsers } from "react-icons/im";
+import { MdOutlineBookmarkAdded } from "react-icons/md";
 
 const DashBoard = () => {
-    return (
-        <div className='w-auto min-h-[100vh] bg-sky-950 '>
-             <div className=''>
-             <Navber></Navber>
-             </div>
-              <div className='flex text-white'>
-             
-            {/* sidebar content */}
-            <div className='w-64 min-h-[100vh] bg-sky-600 mt-28'>
-                <ul className='menu p-4'>
-                <Link to='/dashboard/homes'> <li className='rounded-none mt-4 text-black text-xl'>  Home's</li> </Link>
-                   <Link to='/dashboard/users'> <li className='rounded-none mt-4 text-xl'>User's</li></Link>
- 
-                   <Link to='/dashboard/users'> <li className='rounded-none mt-4 text-xl'>Booking's</li></Link>
-                   <Link to='/dashboard/users'> <li className='rounded-none mt-4 text-xl'>Schedule's</li></Link>
-                </ul>
-            </div>
-            <div className='flex mt-28 bg-white w-full'>
-              
-                <Outlet></Outlet>
-            </div>
-            
+  const isAdmin = true;
+  return (
+    <div className="w-auto min-h-[100vh] bg-sky-950 ">
+      <div className="">
+        <Navber></Navber>
+      </div>
+      <div className="flex text-white">
+        {/* sidebar content */}
+        <div className="w-64 min-h-[100vh] bg-sky-950 mt-28">
+          <ul className="menu p-4">
+            {isAdmin ? (
+              <>
+                <li className="rounded-none  text-white text-xl">
+                  {" "}
+                  <Link to="/dashboard/homes">
+                    <FaHouse></FaHouse> Home's
+                  </Link>
+                </li>
+                <li className="rounded-none  text-white text-xl">
+                  {" "}
+                  <Link to="/dashboard/users">
+                    <ImUsers /> User's
+                  </Link>
+                </li>
+                <li className="rounded-none  text-white text-xl">
+                  {" "}
+                  <Link to="/dashboard/homes">
+                    <MdOutlineBookmarkAdded /> Booking's
+                  </Link>
+                </li>
+                <li className="rounded-none  text-white text-xl">
+                  {" "}
+                  <Link to="/dashboard/homes">
+                    <AiTwotoneSchedule /> Schedule's
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <></>
+            )}
+          </ul>
         </div>
+        <div className="flex mt-28 bg-white w-full">
+          <Outlet></Outlet>
         </div>
-      
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
