744eac81783b897dba729003d0e6d62ff3d0015e
faria-r <fariacse11@gmail.com>
2024-05-23 11:24:14 +0600

    fix: admin dashboard style

--- src/Pages/CommonDashboard/CommonDashboard.jsx
+++ src/Pages/CommonDashboard/CommonDashboard.jsx
@@ -3,11 +3,16 @@ import { useContext } from "react";
 import { AuthContext } from "../../Context/AuthProvider";
 import { FcInvite, FcPhone } from "react-icons/fc";
 import { Link } from "react-router-dom";
-
+import useAdmin from "../../Hooks/useAdmin";
+import banner from '../../assets/k.png'
 const CommonDashboard = () => {
   const { user } = useContext(AuthContext);
+  const [isAdmin]= useAdmin();
   return (
-    <div data-aos="zoom-in-left" data-aos-duration="2000" className=" lg:w-[25vw] p-4 max-h-[50vh] mx-auto mt-16 bg-sky-800  text-center mb-16 shadow-xl text-white">
+    <div data-aos="zoom-in-left" data-aos-duration="2000" className=" lg:w-[25vw] p-4 lg:max-h-[50vh] mx-auto mt-16 bg-sky-800  text-center mb-16 shadow-xl text-white">
+      {
+        isAdmin ?(<div  className="absolute -left-[5%]  text-sky-950 -rotate-[45deg] w-24 text-center "><img src={banner} alt="" /><p className="absolute top-[19%] font-semibold bottom-0 text-center left-[28%]">Admin</p></div>):(<div  className=" text-sky-950  text-center p-[6px] "><img src={banner} alt="" /><p>User</p></div>) 
+      }
       <div className="avatar   mx-auto">
   <div className="w-16 mask mask-hexagon mx-auto">
     <img src={user.photoURL}/>
